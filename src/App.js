c1c1d98feae624f5752a11b1ed5d3719a802f2cd
opsera-eslatt <131897365+opsera-eslatt@users.noreply.github.com>
2023-07-19 10:25:42 -0400

    prepares for worksession

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to Opsera!</p>
+        <p>Cisco, Welcome to Opsera!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
