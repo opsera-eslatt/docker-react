04687aac46d0bebde5626bae6079a75ac3e293bb
opsera-eslatt <131897365+opsera-eslatt@users.noreply.github.com>
2023-07-25 08:27:11 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Coherent, Welcome to Opsera!</p>
+        <p>Welcome to Opsera!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
