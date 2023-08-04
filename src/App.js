3ce5a27b9ebdc480ea55bd23fe44b6117b1e691d
opsera-gimartinb <109554402+opsera-gimartinb@users.noreply.github.com>
2023-08-04 00:02:49 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to Carpus!</p>
+        <p>Welcome to Carpus Watches!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
