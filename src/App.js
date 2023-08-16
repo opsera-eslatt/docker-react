11415ce4ca05b7c94789e785b02287cb2aaa4ce4
opsera-eslatt <131897365+opsera-eslatt@users.noreply.github.com>
2023-08-16 07:09:34 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 1549
+          Learn React 0709
         </a>
       </header>
     </div>
