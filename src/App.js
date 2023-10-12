43d4025b9fef98f0a9c8335b011d590853983829
opsera-eslatt <131897365+opsera-eslatt@users.noreply.github.com>
2023-10-12 09:43:19 -0400

    contrives additional change

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 0939
+          Learn React 0943
         </a>
       </header>
     </div>
